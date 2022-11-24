9ef2cb232edfe5d427ab5311bf23272da83fdeb6
Elvira-del <ninelostr@gmail.com>
2022-11-24 18:15:53 +0300

    feat: add navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Профиль</a>
+        </li>
+        <li>
+          <a href="/">Сообщения</a>
+        </li>
+        <li>
+          <a href="/">Новости</a>
+        </li>
+        <li>
+          <a href="/">Музыка</a>
+        </li>
+        <li>
+          <a href="/">Настройки</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
