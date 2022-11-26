64eec16854abc9939f9dbdc6a465b8fd8231dae2
Elvira-del <ninelostr@gmail.com>
2022-11-26 16:46:16 +0300

    feat: add logo for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import logo from "../../assets/logo.svg";
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="container">
+      <div className="container header__wrap">
+        <img className="logo" src={logo} width="50" height="50" alt="" />
         <Navbar />
       </div>
     </header>
