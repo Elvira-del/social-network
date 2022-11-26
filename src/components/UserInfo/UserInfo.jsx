ef85d2912efa515f12671a2e111a472367e3af00
Elvira-del <ninelostr@gmail.com>
2022-11-27 02:55:05 +0300

    feat: add user info style

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -3,16 +3,18 @@ import React from "react";
 const UserInfo = (props) => {
   return (
     <section className="section info">
-      <div className="container info__wrap">
-        <img className="info__avatar" src={props.avatar} alt="User avatar" />
+      <div className="container">
+        <div className="info__wrap">
+          <img className="info__avatar" src={props.avatar} alt="User avatar" />
 
-        <div className="info__content">
-          <h1 className="title">{props.name}</h1>
-          <ul className="info__list">
-            <li className="info__item">Date of Birth: {props.date}</li>
-            <li className="info__item">City: {props.city}</li>
-            <li className="info__item">Education: {props.education}</li>
-          </ul>
+          <div className="info__content">
+            <h1 className="info__title">{props.name}</h1>
+            <ul className="info__list">
+              <li className="info__item">Date of Birth: {props.date}</li>
+              <li className="info__item">City: {props.city}</li>
+              <li className="info__item">Education: {props.education}</li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
