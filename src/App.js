e0d399c5bbd5cc5b0dc3796159a3ed04d7c56046
Elvira-del <ninelostr@gmail.com>
2022-11-27 15:17:12 +0300

    feat: add footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Navbar />
       <Profile />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span className="footer__copy">© 2022</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
