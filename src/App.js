e159e5d712352591995213a3c2c7a6dbbfbbec9e
Elvira-del <ninelostr@gmail.com>
2022-11-28 21:02:33 +0300

    feat: add React Router for App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
 import Footer from "./components/Footer/Footer";
+import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Navbar />
-      <Profile />
-      <Footer />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Navbar />
+
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/dialogs" element={<Dialogs />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
