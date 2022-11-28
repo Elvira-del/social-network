d9abd3535817b3c42e97172312009c44275f33d8
Elvira-del <ninelostr@gmail.com>
2022-11-28 17:51:13 +0300

    feat: add href for links to pages and translate nav links into English

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,28 +6,28 @@ const Navbar = () => {
       <nav className="nav">
         <ul className="nav__list">
           <li className="nav__item">
-            <a className="nav__link" href="/">
-              Профиль
+            <a className="nav__link" href="/profile">
+              Profile
             </a>
           </li>
           <li className="nav__item">
-            <a className="nav__link" href="/">
-              Сообщения
+            <a className="nav__link" href="/dialogs">
+              Messages
             </a>
           </li>
           <li className="nav__item">
-            <a className="nav__link" href="/">
-              Новости
+            <a className="nav__link" href="/news">
+              News
             </a>
           </li>
           <li className="nav__item">
-            <a className="nav__link" href="/">
-              Музыка
+            <a className="nav__link" href="/music">
+              Music
             </a>
           </li>
           <li className="nav__item">
-            <a className="nav__link" href="/">
-              Настройки
+            <a className="nav__link" href="/settings">
+              Settings
             </a>
           </li>
         </ul>
