2656bc83aac052708f4cfeaad4d96fde47d981f5
Elvira-del <ninelostr@gmail.com>
2022-11-29 00:40:50 +0300

    feat: create dialogs component

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dialogs = () => {
+  return (
+    <div className="container">
+      <section className="section dialogs">
+        <h2 className="subtitle">Dialogs</h2>
+      </section>
+    </div>
+  );
+};
+
+export default Dialogs;
