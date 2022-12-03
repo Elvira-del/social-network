996e35e2a6347aa885c9f29bae75e38ceb9f9de9
Elvira-del <ninelostr@gmail.com>
2022-12-03 17:58:58 +0300

    fix: transfer useEffect hook to Dialogs component

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,42 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ContactItem from "../ContactItem/ContactItem";
 
-const Contacts = () => {
-  const [contacts, setContacts] = useState([]);
-
-  // useEffect(() => {
-  //   fetch("https://dummyapi.io/data/v1/user?limit=10", {
-  //     method: "GET",
-  //     headers: { "app-id": "638754260803155fa32416a8" },
-  //   })
-  //     .then((res) => res.json())
-  //     .then((json) => {
-  //       setContacts(json.data);
-  //     })
-  //     .catch((error) => {
-  //       console.warn(error);
-  //       alert("Произошла ошибка при загрузке данных.");
-  //     });
-  // }, []);
-
-  useEffect(() => {
-    fetch("https://app.fakejson.com/q/a2kbQT6W?token=EPPceshX_h8X0L9YzasBwA", {
-      method: "GET",
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        setContacts(json);
-      })
-      .catch((error) => {
-        console.warn(error);
-        alert("Произошла ошибка при загрузке данных.");
-      });
-  }, []);
-
+const Contacts = (props) => {
   return (
     <div className="contacts">
       <ul className="contacts__list">
-        {contacts.map((item, idx) => (
+        {props.contacts.map((item, idx) => (
           <ContactItem
             key={idx}
             id={item.id}
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Contacts from "../Contacts/Contacts";
+import Messages from "../Messages/Messages";
 
 const Dialogs = () => {
+  const [dialogs, setDialogs] = useState([]);
+
+  useEffect(() => {
+    fetch("https://636af450b10125b78fe83042.mockapi.io/dialogs")
+      .then((res) => res.json())
+      .then((json) => {
+        setDialogs(json);
+      })
+      .catch((error) => {
+        console.warn(error);
+        alert("Произошла ошибка при загрузке данных.");
+      });
+  }, []);
+
   return (
     <div className="container">
       <section className="section dialogs">
         <h2 className="subtitle">Dialogs</h2>
 
         <div className="dialogs__wrap">
-          <Contacts />
+          <Contacts contacts={dialogs} />
+          <Messages messages={dialogs} />
         </div>
       </section>
     </div>
