8e618cf1443a1ee2e839eade52fb81a81ef89820
Elvira-del <ninelostr@gmail.com>
2022-12-05 20:02:14 +0300

    feat: add tracking user input to new post  textarea and create a new post

--- src/components/MyPosts/MyPosts.jsx
+++ src/components/MyPosts/MyPosts.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import Post from "../Post/Post";
 import avatar from "../../assets/images/avatar.jpg";
 
 const MyPosts = () => {
+  const [textPost, setTextPost] = useState("");
+  const newPostContainer = useRef(null);
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    setTextPost(newPostContainer.current.value);
+  };
+
   return (
     <section className="section posts">
       <div className="posts__wrap">
         <h2 className="subtitle">My posts</h2>
+
         <form className="posts__form form" action="#" method="post">
           <textarea
             className="form__input"
+            ref={newPostContainer}
             cols="30"
             rows="5"
             placeholder="Your text..."
             autoCorrect="on"
           ></textarea>
-          <button className="btn form__btn" type="submit">
+          <button className="btn form__btn" type="submit" onClick={addNewPost}>
             Send
           </button>
         </form>
-        <Post avatar={avatar} content="Hi! What do you think?" />
-        <Post avatar={avatar} content="My first post" />
+
+        {textPost !== "" && <Post avatar={avatar} content={textPost} />}
       </div>
     </section>
   );
