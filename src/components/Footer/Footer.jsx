d1244182bb83417649863ec01b5d95d4e9e54151
Elvira-del <ninelostr@gmail.com>
2022-12-08 19:51:42 +0300

    refactor: add Footer scss module file

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import style from "./Footer.module.scss";
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className={style.footer}>
       <div className="container">
-        <span className="footer__copy">© 2022</span>
+        <span className={style.footer__copy}>© 2022</span>
       </div>
     </footer>
   );
