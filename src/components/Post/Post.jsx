3808080b6616726e4db56d75a6d7c1a7a60aae06
Elvira-del <ninelostr@gmail.com>
2022-12-08 21:29:10 +0300

    refactor: add MyPosts scss module file

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Post = (props) => {
   return (
-    <div className="post">
-      <img className="post__avatar" src={props.avatar} alt="" />
-      <p className="post__text">{props.content}</p>
+    <div className={props.style.post}>
+      <img className={props.style.post__avatar} src={props.avatar} alt="" />
+      <p className={props.style.post__text}>{props.content}</p>
     </div>
   );
 };
