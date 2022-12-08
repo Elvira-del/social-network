1a2ba02224113280c2b7f6f9b12c4f2196f87ebd
Elvira-del <ninelostr@gmail.com>
2022-12-08 19:38:21 +0300

    refactor: add Contacts scss module file

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ContactItem from "../ContactItem/ContactItem";
+import style from "./Contacts.module.scss";
 
 const Contacts = (props) => {
+  console.log(style);
   return (
     <div className="contacts">
-      <ul className="contacts__list">
+      <ul className={style.contacts__list}>
         {props.contacts.map((item, idx) => (
           <ContactItem
             key={idx}
+            style={style}
             id={item.id}
             name={item.name}
             avatar={item.avatar}
