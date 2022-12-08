2b253a3affacf51ca61ca5da65ed0b2db834b9df
Elvira-del <ninelostr@gmail.com>
2022-12-08 19:33:57 +0300

    Revert "feat: add Contacts scss module with @use rule"
    This reverts commit 9648bc82ee49b2196c253ccd7a1eb4e70bd33aa4.

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -2,13 +2,9 @@ import { Link } from "react-router-dom";
 
 const ContactItem = (props) => {
   return (
-    <li className={props.style.contacts__item}>
-      <img
-        className={props.style.contacts__icon}
-        src={props.avatar}
-        alt="Friend avatar"
-      />
-      <Link className={props.style.contacts__link} to={`/dialogs/${props.id}`}>
+    <li className="contacts__item">
+      <img className="contacts__icon" src={props.avatar} alt="Friend avatar" />
+      <Link className="contacts__link" to={`/dialogs/${props.id}`}>
         {props.name}
       </Link>
     </li>
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ContactItem from "../ContactItem/ContactItem";
-import style from "./Contacts.module.scss";
 
 const Contacts = (props) => {
-  console.log(style);
   return (
     <div className="contacts">
-      <ul className={style.contacts__list}>
+      <ul className="contacts__list">
         {props.contacts.map((item, idx) => (
           <ContactItem
             key={idx}
-            style={style}
             id={item.id}
             name={item.name}
             avatar={item.avatar}
