d0fe0fbbdeba52f19521b792e56ee4d511a5b15d
Elvira-del <ninelostr@gmail.com>
2022-12-12 22:18:41 +0300

    feat: add textarea for Messages

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -3,9 +3,23 @@ import React from "react";
 const Messages = (props) => {
   return (
     <div className="messages">
-      {props.messages.map((item, idx) => (
-        <p key={idx}>{item.stringShort}</p>
-      ))}
+      <div className="messages__content">
+        {props.messages.map((item, idx) => (
+          <p className="messages__text" key={idx}>
+            {item.stringShort}
+          </p>
+        ))}
+      </div>
+
+      <form className="messages__form" action="#" method="post">
+        <textarea
+          className="messages__input"
+          cols="30"
+          rows="5"
+          autoCorrect="on"
+        />
+        <button className="btn messages__btn">Send</button>
+      </form>
     </div>
   );
 };
